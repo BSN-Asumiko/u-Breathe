0ac6048cb14fbae6971d9a35860f4b733d23c797
helopgom <helena.lopez.gomez@gmail.com>
2024-07-04 12:29:15 +0200

    FormPage check with component FormInput and styles

---
+++ src/components/formInput/FormInput.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FormInput = ({ id, label, register, requiredMessage, error }) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={id}>{label}</label>
+      <textarea id={id} {...register(id, { required: requiredMessage })} />
+      {error && <div className="popup">{error.message}</div>}
+    </div>
+  );
+};
+
+export default FormInput;
